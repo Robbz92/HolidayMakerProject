18c81467058a60aeef238bda13c33b82375be24b
Andreas <Andreas_93l@hotmail.com>
2021-05-03 15:24:45 +0200

    BÄTTRE config.js

--- Frontend/holidaymakerfrontend/vue.config.js
+++ Frontend/holidaymakerfrontend/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
     devServer: {
         port: 3000,
         proxy: {
+            '/rest': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true,
+                secure:false
+            },
          
         }
     }
