005f2e8cdba4c3519c7a5d710fc436d7a421899a
Martin Jönsson <mr_tines_j@hotmail.com>
2021-05-20 09:51:26 +0200

    This time with Comments™!

--- Frontend/holidaymakerfrontend/src/components/StripeCheckout.vue
+++ Frontend/holidaymakerfrontend/src/components/StripeCheckout.vue
@@ -9,12 +9,14 @@
             :cancel-url="cancelURL"
             @loading="v => loading = v"
         />
+        <!-- The almighty "Pay now!" button -->
         <button @click="submit">Pay now!</button>
     </div>
 </template>
 
 <script>
 import { StripeCheckout } from '@vue-stripe/vue-stripe'
+
 export default {
     components:{
         StripeCheckout
@@ -23,21 +25,28 @@ export default {
     props:['totalPrice'],
 
     data(){
+        //Assigning the test API key connected to HolidayMaker
         this.publishableKey = 'pk_test_51IsnNBFc0ZEqxGhpEsIHDP9Iref72shtu3j1X4vKjyBYW2zsZYovgbMYNYkdLjfZwo7rMfj9IwqBskqChSQlMa0V00oNXRjzCf';
         return{
             loading: false,
+            //Defining products, which in this case is only one
             lineItems: [
                 {
+                    //The ID for said product
                     price: 'price_1Isny4Fc0ZEqxGhpxKl5IZgV',
+                    //Using quantity instead of price, for simplicity
                     quantity: this.totalPrice
                 },
                 ],
+            //Redirecting you to the 'Success' page, if payment is successful
             successURL: 'http://localhost:3000/success',
+            //Redirecting you to the 'Cancel' page, if payment is declined or canceled
             cancelURL: 'http://localhost:3000/cancel',
         }
     },
     methods:{
         submit(){
+            //Sending you to the checkout page from stripe
             this.$refs.checkoutRef.redirectToCheckout();
         }
     }
