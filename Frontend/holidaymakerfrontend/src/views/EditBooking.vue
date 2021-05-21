c5e0058e83f60a87f8689d17e2ef061d3612c040
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2021-05-21 16:01:08 +0200

    Unfinished business in EditBooking

--- Frontend/holidaymakerfrontend/src/views/EditBooking.vue
+++ Frontend/holidaymakerfrontend/src/views/EditBooking.vue
@@ -1,53 +1,59 @@
 <template>
   <div class="main-container">
-    <div class="hotel-container">
-      <ul v-for="(theBook, index) in getTheBooking" :key="index">
-        <li id="list">
-          <div class="hotelPicture">
-            <img id="hotelPic" :src="theBook.hotel_img" />
-          </div>
-          <h2>{{ theBook.name }}</h2>
-          <p>Total Cost: {{ theBook.total_cost }} kr</p>
-          <p>Date: {{ theBook.from_date }} - {{ theBook.to_date }}</p>
-          <p>Room: {{ theBook.type }}</p>
-          <p>Board: {{ theBook.board }}</p>
-          <p>Address: {{ theBook.address }}</p>
-          <div class="buttons">
-            <button id="deleteBooking" @click="deleteBooking(theBook.id)">Delete Booking</button>
-            <button id="saveBooking">Save</button>
-          </div>
-        </li>
-      </ul>
+    <Book/>
+  </div>
+  <div class="date">
+    <div class="checkIn">
+      <h6>Check-in</h6>
+      <date-picker
+        id="fromDate"
+        v-model="fromDate"
+        language="en"
+        type="date"
+        format="YYYY-MM-DD"
+        :style="styleObject"
+      ></date-picker>
+    </div>
+    <div class="checkOut">
+      <h6>Check-out</h6>
+      <date-picker
+        id="toDate"
+        v-model="toDate"
+        language="en"
+        type="date"
+        format="YYYY-MM-DD"
+        :style="styleObject"
+      ></date-picker>
     </div>
   </div>
 </template>
 
 <script>
+import DatePicker from "vue3-datepicker";
+import Book from "../components/MyBooking.vue";
+
 export default {
-  computed: {
-    getTheBooking() {
-      return this.$store.getters.getClickedBooking;
-    },
+  components: {
+    Book,
+    DatePicker,
   },
 
-  methods: {
-      deleteBooking(id) {
-          if(confirm("Are you sure you want to delete the booking?")){
-              this.$store.dispatch("fetchDeleteBooking", id);
-              this.$router.push("/myBookings");
-          }
+  data() {
+    return {
+      fromDate: new Date,
+      toDate: new Date,
+      styleObject: {
+        outline: "none",
+        border: "none",
+        height: "1.65em",
+        width: "10em",
+        fontFamily: "inherit",
       },
-  }
+    };
+  },
+
 };
 </script>
 
 <style scoped>
-.hotel-container {
-  background-color: white;
-}
-
-#list {
-  list-style: none;
-  display: flex;
-}
 </style>
