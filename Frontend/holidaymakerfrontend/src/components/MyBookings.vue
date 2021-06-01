89e26c6dd61f8d086a2dfba84ea8d16282a93c63
Robin <rp.ullstorp92@gmail.com>
2021-06-01 13:18:33 +0200

    kan bara edit booking om datumet inte har passerat

--- Frontend/holidaymakerfrontend/src/components/MyBookings.vue
+++ Frontend/holidaymakerfrontend/src/components/MyBookings.vue
@@ -1,35 +1,54 @@
 <template>
+  {{ currentDate() }}
   <div class="main-container" v-if="getAllMyBookings != ''">
-    <EditBooking v-if="show" :fromDate="fromDate" :toDate="toDate" :board="board" :extra_bed_amount="extra_bed_amount" :type="type"
-    :total_cost="total_cost" :room_img="room_img" :hotel_img="hotel_img" :name="name" :id="id"/>
+    <EditBooking
+      v-if="show"
+      :fromDate="fromDate"
+      :toDate="toDate"
+      :board="board"
+      :extra_bed_amount="extra_bed_amount"
+      :type="type"
+      :total_cost="total_cost"
+      :room_img="room_img"
+      :hotel_img="hotel_img"
+      :name="name"
+      :id="id"
+    />
     <div class="bookings" v-if="!show">
       <ul v-for="(bookings, index) in getAllMyBookings" :key="index">
-        <li id="booking"> 
+        <li id="booking">
           <div class="hotelPicture">
-             <h2>{{ bookings.name }}</h2>
+            <h2>{{ bookings.name }}</h2>
             <img id="hotelPic" :src="bookings.hotel_img" />
           </div>
           <div class="booking-text">
             <table>
-            <tr>
-              <th>Address</th>
-              <th>From Date</th>
-              <th>To Date</th>
-              <th>Booked Rooms</th>
-              <th>Total Cost</th>
-            </tr>
-            <tr>
-              <td>{{bookings.address}}</td>
-              <td>{{ bookings.from_date }}</td>
-              <td>{{ bookings.to_date }}</td>
-              <td>{{ bookings.BookedRooms}}</td>
-              <td>{{ bookings.total_cost }}</td>
-            </tr>            
-          </table>
+              <tr>
+                <th>Address</th>
+                <th>From Date</th>
+                <th>To Date</th>
+                <th>Booked Rooms</th>
+                <th>Total Cost</th>
+              </tr>
+              <tr>
+                <td>{{ bookings.address }}</td>
+                <td>{{ bookings.from_date }}</td>
+                <td>{{ bookings.to_date }}</td>
+                <td>{{ bookings.BookedRooms }}</td>
+                <td>{{ bookings.total_cost }}</td>
+              </tr>
+            </table>
           </div>
-          <div class="buttons-container" >
-            <button v-if="checkIfReviewed(bookings.hotel_id)" @click="sendBookingId(bookings.id)">Review</button>
-            <button @click="editBooking(bookings)">Edit</button>
+          <div class="buttons-container">
+            <button
+              v-if="checkIfReviewed(bookings.hotel_id)"
+              @click="sendBookingId(bookings.id)"
+            >
+              Review
+            </button>
+            <button v-if="checkIfOutOfDate(index)" @click="editBooking(bookings)">
+              Edit
+            </button>
           </div>
         </li>
       </ul>
@@ -43,85 +62,97 @@
 <script>
 import EditBooking from "../views/EditBooking.vue";
 export default {
-    components: {
-        EditBooking
+  components: {
+    EditBooking,
+  },
+
+  data() {
+    return {
+      score: 0,
+      text: "",
+      hotel_id: "",
+      show: false,
+      fromDate: "",
+      toDate: "",
+      board: "",
+      name: "",
+      hotel_img: "",
+      extra_bed_amount: "",
+      type: "",
+      room_img: "",
+      total_cost: "",
+      id: "",
+    };
+  },
+
+  computed: {
+    getAllMyBookings() {
+      return this.$store.getters.getMyBookings;
+    },
+    getRoomsForEdit() {
+      return this.$store.getters.getRoomsForEdit;
     },
 
-    data() {
-        return {
-            score: 0,
-            text: "",
-            hotel_id: "",
-            show: false,
-            fromDate: "",
-            toDate: "",
-            board:"",
-            name:"",
-            hotel_img:"",
-            extra_bed_amount:"",
-            type:"",
-            room_img:"",
-            total_cost:"",
-            id:"",
-        };
+    getHotelList() {
+      return this.$store.getters.getHotelListForReview;
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch("fetchMyBookings");
+    this.$store.dispatch("fetchHotelListForReviews");
+  },
+
+  methods: {
+    checkIfOutOfDate(index) {
+      // formaterar datum samt kollar om datumet har gått ut, såfall ska man inte kunna editera något.
+      if (this.getAllMyBookings[index].from_date > this.currentDate()) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    // Dagens datum
+    currentDate() {
+      var date = new Date().toISOString().slice(0, 10);
+      return date;
     },
 
-    computed: {
-        getAllMyBookings() {
-            return this.$store.getters.getMyBookings;
-        },
-        getRoomsForEdit(){
-            return this.$store.getters.getRoomsForEdit;
-        },
-
-        getHotelList(){
-          return this.$store.getters.getHotelListForReview;
-        },
-
+    checkIfReviewed(hotelId) {
+      if (this.$store.getters.getHotelListForReview.includes(hotelId)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    sendFromDate(fromDate) {
+      this.$store.commit("setFromDate", fromDate);
+    },
+    sendToDate(toDate) {
+      this.$store.commit("setToDate", toDate);
+    },
+    sendBookingId(bookingId) {
+      this.$store.dispatch("fetchClickedBooking", bookingId);
+      this.$router.push("/review");
     },
 
-    mounted() {
-        this.$store.dispatch("fetchMyBookings");
-        this.$store.dispatch("fetchHotelListForReviews");
+    editBooking(booking) {
+      this.fromDate = booking.from_date;
+      this.toDate = booking.to_date;
+      this.board = booking.board;
+      this.name = booking.name;
+      this.hotel_img = booking.hotel_img;
+      this.extra_bed_amount = booking.extra_bed_amount;
+      this.type = booking.type;
+      this.room_img = booking.room_img;
+      this.total_cost = booking.total_cost;
+      this.id = booking.id;
+      this.$store.dispatch("fetchBookedRoom", booking.id);
+      this.$store.dispatch("fetchClickedBooking", booking.id);
+      this.show = true;
     },
-
-    methods: {
-      checkIfReviewed(hotelId){
-        if(this.$store.getters.getHotelListForReview.includes(hotelId)){
-          return false;
-        }
-        else{
-          return true;
-        }
-      },
-        sendFromDate(fromDate){
-            this.$store.commit("setFromDate", fromDate);
-        },
-        sendToDate(toDate){
-            this.$store.commit("setToDate", toDate);
-        },
-        sendBookingId(bookingId) {
-            this.$store.dispatch("fetchClickedBooking", bookingId);
-            this.$router.push("/review");
-        },
-
-        editBooking(booking) {
-            this.fromDate = booking.from_date;
-            this.toDate = booking.to_date;
-            this.board=booking.board;
-            this.name = booking.name;
-            this.hotel_img = booking.hotel_img;
-            this.extra_bed_amount = booking.extra_bed_amount;
-            this.type = booking.type;
-            this.room_img = booking.room_img;
-            this.total_cost = booking.total_cost;
-            this.id=booking.id;
-            this.$store.dispatch("fetchBookedRoom", booking.id);
-            this.$store.dispatch("fetchClickedBooking", booking.id);
-            this.show = true;
-        },
-    },
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -135,7 +166,6 @@ export default {
   margin: 0 auto;
   margin-top: 5em;
   backdrop-filter: blur(5px);
-  
 }
 #booking {
   margin-top: 0;
@@ -144,16 +174,16 @@ export default {
   display: flex;
 }
 
-.booking-text{
+.booking-text {
   width: 100%;
   margin-top: 70px;
 }
 
-.booking-text table{
-  width:100%;
+.booking-text table {
+  width: 100%;
 }
 
-.booking-text table th{
+.booking-text table th {
   margin: 0;
 }
 
@@ -177,15 +207,14 @@ h4 {
   width: 100%;
 }
 
-.buttons-container{
-  display:flex;
+.buttons-container {
+  display: flex;
 }
 
-.buttons-container button{
-  width:75px;
+.buttons-container button {
+  width: 75px;
   height: 40px;
   margin-top: 63px;
   margin-right: 10px;
 }
-
 </style>
